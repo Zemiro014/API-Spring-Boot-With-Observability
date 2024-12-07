f930ffe3078eb77ac5ce8f9273feea6a37b203e1
zemiro014 <zemiro014@gmail.com>
2024-12-07 18:30:59 -0500

    first commit

---
+++ src/main/java/com/jeronimo/service_with_observability/ServiceWithObservabilityApplication.java
@@ -0,0 +1,13 @@
+package com.jeronimo.service_with_observability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceWithObservabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceWithObservabilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeronimo/service_with_observability/ServiceWithObservabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeronimo.service_with_observability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceWithObservabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
